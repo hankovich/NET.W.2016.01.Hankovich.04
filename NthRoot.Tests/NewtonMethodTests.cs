64ab7456853bad1b636a197546c37a33dc8887ca
hankovich <svyatoslav.hankovich@gmail.com>
2016-10-27 13:08:54 +0300

    Init commit

---
+++ NthRoot.Tests/NewtonMethodTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NthRoot.Tests
+{
+    [TestClass]
+    public class NewtonMethodTests
+    {
+        [TestMethod]
+        public void NthRoot_sqrt2_Eps6Returned()
+        {
+            //A
+            var expected = 1.4142;
+            //A
+            var actual = NewtonMethod.NthRoot(2, 2, 4);
+
+            //A
+            Debug.WriteLine(expected - actual);
+            Assert.IsTrue(condition: Math.Abs(expected - actual) < Math.Pow(10, -4));
+        }
+
+        [TestMethod]
+        public void NthRoot_3thminus1000_Eps15Returned()
+        {
+            //A
+            double expected = -10;
+            //A
+            var actual = NewtonMethod.NthRoot(-1000, 3, 15);
+
+            //A
+
+            Debug.WriteLine(expected - actual);
+            Assert.IsTrue(condition: Math.Abs(expected - actual) < Math.Pow(10, -15));
+        }
+
+        [TestMethod]
+        public void NthRoot_maxvalue_Eps15Returned()
+        {
+            //A
+            var expected = Math.Pow(double.MaxValue, 0.01);
+            //A
+            var actual = NewtonMethod.NthRoot(double.MaxValue, 100, 15);
+
+            //A
+
+            Debug.WriteLine(expected - actual);
+            Assert.IsTrue(condition: Math.Abs(expected - actual) < Math.Pow(10, -15));
+        }
+
+        [TestMethod]
+        public void NthRoot_3rdrootminus3_exceptionReturned()
+        {
+            var actual = NewtonMethod.NthRoot(-125, 3, 6);
+            double expected = -5;
+
+            Debug.WriteLine(expected - actual);
+            Assert.IsTrue(condition: Math.Abs(expected - actual) < Math.Pow(10, -6));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NthRoot_zero_zeroReturned()
+        {
+            var actual = NewtonMethod.NthRoot(0, 2, 15);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NthRoot_sqrtminus2_exceptionReturned()
+        {
+            var actual = NewtonMethod.NthRoot(-2, 2, 6);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NthRoot_sqrt2epsMinus1000_exceptionReturned()
+        {
+            var actual = NewtonMethod.NthRoot(2, 2, -100);
+            
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void NthRoot_sqrt2_sqrt2AccuracyEps16Returned()
+        {
+           var actual = NewtonMethod.NthRoot(2, 2, 16);
+        }
+    }
+}
---
+++ NthRoot/NewtonMethod.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NthRoot
+{
+    public static class NewtonMethod
+    {
+        /// <summary>
+        /// Find nth root by using Newton's method.
+        /// </summary>
+        /// <param name="x">Number to find a root of</param>
+        /// <param name="n">Degree of a root</param>
+        /// <param name="eps">Accuracy 10^(-eps)</param>
+        /// <exception cref="InvalidOperationException"> When n is less or equal than 0</exception>
+        /// <exception cref="InvalidOperationException"> When eps is less or equal than 0 </exception>
+        /// <exception cref="InvalidOperationException"> When x = 0 </exception>
+        /// <exception cref="InvalidOperationException"> When you try to get even root of a negative number</exception>
+        /// <returns></returns>
+        public static double NthRoot(double x, int n, int eps)
+        {
+            if (n <= 0 || eps <= 0 || eps >= 16 || x == 0)
+                throw new InvalidOperationException();
+
+            if (x < 0 && n%2 == 0)
+                throw new InvalidOperationException();
+
+            double result;
+            var newResult = x/n;
+            var accuracy = Math.Pow(10, -eps);
+            do
+            {
+                result = newResult;
+                newResult = ((n - 1)*result + x/Math.Pow(result, n - 1))/n;
+            } while (Math.Abs(result - newResult) > accuracy);
+            return newResult;
+        }
+    }
+}
